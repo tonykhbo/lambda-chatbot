dd080fb30168c48057fd7db161a583b0c3871809
Tony Bo <tonykhbo@gmail.com>
2022-11-22 20:59:34 -0500

    updated readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # lambda-chatbot
+
+This is a WebEx chatbot that leverages AWS Lambda functions to listen for messages and responds to them.
+
+### Setup & Installation
+
+Install serverless package via npm. We'll use this to deploy our functions to AWS Lambda.
+
+```
+npm install -g serverless
+```
+Clone the repo
+
+```
+git clone https://github.com/tonykhbo/lambda-chatbot.git
+```
+
+### AWS Credentials Setup
+
+Follow the instructions in the following links to create an AWS Access Key: 
+
+    - https://www.serverless.com/framework/docs/providers/aws/guide/credentials
+
+    - [Video Guide on setting up credentials](https://www.youtube.com/watch?v=KngM5bfpttA)
+
+Export them as environment variables so they would be accessible to Serverless and the AWS SDK in your shell:
+
+```
+export AWS_ACCESS_KEY_ID=<your-key-here>
+export AWS_SECRET_ACCESS_KEY=<your-secret-key-here>
+```
+
+### Set Environment Variables in .env file
+
+Modify the .env file: 
+
+```
+functions:
+  WEBEX_CHATBOT_TOKEN=changethis_12345678abcdefg
+```
+
+### Deploy
+
+Change directory to the project root 
+
+```
+cd lambda-chatbot
+```
+
+Deploy the functions to AWS Lambda:
+
+```
+serverless deploy
+```
+
+### Modifying Environment variables after Deployment
+
+    - Open the Functions page of the Lambda console.
+    - Choose a function.
+    - Choose Configuration, then choose Environment variables.
+    - Under Environment variables, choose Edit.
+    - Choose Add environment variable.
+    - Enter a key and value.
+    - Choose Save.
--- handler.py
+++ handler.py
@@ -1,24 +1,47 @@
-import json
+import json, os
+import requests
+import pandas as pd
 
+TOKEN = os.environ['WEBEX_CHATBOT_TOKEN']
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+def getMessage(event):
+    print(event)
+    url = "https://webexapis.com/v1/messages/" + event["data"]["id"]
+    headers = {
+        "Authorization": "Bearer "+ TOKEN,  # Bot's access token
+        "Content-Type": "application/json"
     }
+    
+    response = requests.request("GET", url=url, headers=headers)
+    return response.json()
+    
+def checkCSV(messageDetails):
+    user_input = messageDetails["text"]
+    SHEET_ID = '1LrhboEChYbmjTACMWDYlN0Q9fPKv9JodANNRo43OY8s'
+    SHEET_NAME = 'data'
+    url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+    df = pd.read_csv(url)
+    #print(df.head())
+    contactInfo = df[df["name"] == user_input]["ContactInfo"].iloc[0]
+    return contactInfo
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+def postResponse(messageDetails, contactInfo):
+    if (messageDetails["personEmail"] != "vumi@webex.bot"):
+        url = "https://webexapis.com/v1/messages"
+        headers = {
+            "Authorization": "Bearer "+ TOKEN,
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "roomId": messageDetails["roomId"],
+            "text": contactInfo
+        }
+    
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
 
-    return response
+def webhook(event, context):
+    messageDetails = getMessage(json.loads(event["body"]))
+    contactInfo = checkCSV(messageDetails)
+    postResponse(messageDetails, contactInfo)
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    return event
